f7cb776ec265645b7351376c2651fb02584fd3b0
Jayson Rowe <jayson.rowe@gmail.com>
2012-09-09 22:07:42 -0400

    added files

---
+++ GmailTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TestingSeleniumGmail
+{
+    [TestClass]
+    public class FillFormIntegrationTest
+    {
+        public static string BaseUrl = "http://www.gmail.com";
+        public const int TimeOut = 30;
+
+        [TestMethod]
+        public void CanFillAndSubmitFormAfterLogin()
+        {
+            var driver = new FirefoxDriver();
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(TimeOut));
+
+            driver.Navigate().GoToUrl(BaseUrl);
+
+            driver.FindElement(By.Id("Email")).SendKeys("email.address@gmail.com");
+            driver.FindElement(By.Id("Passwd")).SendKeys("!SuperSecretpassw0rd");
+
+            driver.FindElement(By.Id("signIn")).Click();
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+
+            driver.SwitchTo().Frame("canvas_frame");
+            driver.FindElement(By.CssSelector("div[class='T-I J-J5-Ji T-I-KE L3']")).Click();
+
+
+            driver.FindElement(By.CssSelector("textarea[class='dK nr']")).SendKeys("another.email@somedomain.com");
+            driver.FindElement(By.CssSelector("input[class='ez nr']")).SendKeys("This is a test");
+            driver.FindElement(By.CssSelector("textarea[class='Ak aXjCH']")).SendKeys("This is a test email sent via Selenium Web Driver");
+            driver.FindElement(By.CssSelector("div[class='T-I J-J5-Ji Bq nS T-I-KE L3']>b")).Click();
+
+        }
+    }
+}
---
+++ WatercoolerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Web.UI.Tests
+{
+    [TestClass]
+    public class FillFormIntegrationTest
+    {
+        public static string BaseUrl = "http://acswatercooler.heroku.com";
+        public const int TimeOut = 30;
+
+        [TestMethod]
+        public void CanFillAndSubmitFormAfterLogin()
+        {
+            var driver = new FirefoxDriver();
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(TimeOut));
+
+            driver.Navigate().GoToUrl(BaseUrl + "/signin");
+
+            driver.FindElement(By.Id("session_email")).SendKeys("just.testing@email.com");
+            driver.FindElement(By.Id("session_password")).SendKeys("abc123");
+
+            driver.FindElement(By.Id("session_submit")).Click();
+
+            Assert.AreEqual("ACS Technologies Water Cooler | Just Testing", driver.Title);
+
+            driver.FindElement(By.LinkText("Home")).Click();
+
+            Assert.AreEqual("ACS Technologies Water Cooler | Home", driver.Title);
+
+            driver.FindElement(By.Id("micropost_content")).SendKeys("This is a test from Selenium");
+
+            driver.FindElement(By.Id("micropost_submit")).Click();
+        }
+    }
+}
